708e9b3b681c91ecb46a97a9f169f20186ba7a2f
Guillaume Occuly <occuly.guillaume@gmail.com>
2026-03-29 21:04:56 +0200

    fix: corriger erreur TS sur image.ts (client nullable)

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,7 +1,7 @@
 import { createImageUrlBuilder } from '@sanity/image-url'
-import { client } from './client'
+import { projectId, dataset } from '../env'
+
+const builder = createImageUrlBuilder({ projectId: projectId || '', dataset: dataset || '' })
 
 export const urlFor = (source: Parameters<typeof builder.image>[0]) =>
   builder.image(source)
-
-const builder = createImageUrlBuilder(client)
